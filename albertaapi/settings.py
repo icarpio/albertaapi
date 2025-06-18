891e432316157787a8a9c8f512a1110d1215ab63
Ivan <icarpiodeveloper@gmail.com>
2025-06-18 20:22:37 +0200

    Try Deploy

--- albertaapi/settings.py
+++ albertaapi/settings.py
@@ -9,10 +9,15 @@ load_dotenv()
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('SECRET_KEY')
-DEBUG = True
-#DEBUG = False
+#DEBUG = True
+
+ALLOWED_HOSTS = ['*']
+CSRF_TRUSTED_ORIGINS = ['*']
+
+DEBUG = False
 #ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split(',')
-ALLOWED_HOSTS = []
+#CSRF_TRUSTED_ORIGINS = os.getenv('CSRF_TRUSTED_ORIGINS')
+
 
 
 # Application definition
