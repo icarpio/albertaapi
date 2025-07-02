32e16db51917477a4f7cd733461846816eda339f
Ivan <icarpiodeveloper@gmail.com>
2025-07-02 18:04:44 +0200

    updates

--- cluedo/urls.py
+++ cluedo/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path("case/", views.get_case, name="get_case"),
     path("chat/", views.chat, name="chat"),
     path("accuse/", views.accuse, name="accuse"),
+    path("restart/", views.restart_game_view, name="restart_game"),
 ]
--- cluedo/views.py
+++ cluedo/views.py
@@ -2,11 +2,13 @@ import json
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_GET, require_POST
-
-from .logic import get_npc_response, check_accusation, get_current_case
+from .models import GameState
+import random
+from .logic import get_npc_response, check_accusation, get_current_case,CASES, NPCS
 
 @require_GET
 def get_case(request):
+    print(f"Method: {request.method}")
     case = get_current_case()
     if case:
         return JsonResponse({
@@ -38,3 +40,32 @@ def accuse(request):
 
     correct, message = check_accusation(npc_id)
     return JsonResponse({"correct": correct, "message": message})
+
+
+@csrf_exempt
+@require_POST
+def restart_game_view(request):
+    
+    # Borra todo y crea nuevo estado
+    GameState.objects.all().delete()
+    
+      # o de donde tengas tus datos
+
+    new_case = random.choice(CASES)
+    new_assassin = random.choice(list(NPCS.keys()))
+
+    gs = GameState.objects.create(
+        id=1,
+        case_id=new_case["id"],
+        assassin=new_assassin
+    )
+
+    return JsonResponse({
+        "message": "Nuevo juego iniciado",
+        "case": {
+            "id": new_case["id"],
+            "title": new_case["title"],
+            "details": new_case["details"],
+        }
+        # "assassin": new_assassin  # ⚠️ para debug; no mostrar en frontend real
+    })
