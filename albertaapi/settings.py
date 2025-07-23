49c127eab1f59bad69615ef4a6f4d87ffb51ea95
Ivan <icarpiodeveloper@gmail.com>
2025-07-23 22:46:50 +0200

    Update Admin

--- albertaapi/settings.py
+++ albertaapi/settings.py
@@ -11,7 +11,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Clave secreta para Django
 SECRET_KEY = os.getenv('SECRET_KEY')
 
-DEBUG = True
+DEBUG = False
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '127.0.0.1').split(',')
 CORS_ALLOWED_ORIGINS = os.getenv('CORS_ALLOWED_ORIGINS', '').split(',')
 OPENAI = os.getenv('OPENAI')
